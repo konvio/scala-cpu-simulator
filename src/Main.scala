76005ae0438d9ccdeef35914a298295eed0fe1d4
konv <konvdev@gmail.com>
2016-11-14 17:49:01 +0200

    Add Readme

--- src/Main.scala
+++ src/Main.scala
@@ -3,9 +3,10 @@ object Main extends App {
   val ac = new Register("ac")
   val regs = new Array[Register](4)
   val regR = "r([0-3])".r
-  val numR = "(\\d+)".r
+  val numR = "(\\-?\\d+)".r
 
   def displayState() = {
+    println(ac.getFlags())
     println(ac)
     for (r <- regs) println(r)
   }
@@ -26,6 +27,14 @@ object Main extends App {
     }
   }
 
+  def add(arg: String) = {
+    arg match {
+      case regR(index) => ac add regs(index.toInt)
+      case numR(value) => ac add value.toInt
+      case _ => println("Invalid argument")
+    }
+  }
+
   def copy(arg: String) = {
     arg match {
       case regR(index) => regs(index.toInt) copy ac
@@ -37,11 +46,12 @@ object Main extends App {
 
   while (true) {
     displayState()
-    val command = io.StdIn.readLine.trim.toLowerCase split "[^\\w']+"
+    val command = io.StdIn.readLine.trim.toLowerCase split "\\s+"
     command match {
       case Array("load", _) => load(command(1))
       case Array("xor", _) => xor(command(1))
       case Array("copy", _) => copy(command(1))
+      case Array("add", _) => add(command(1))
       case Array("exit") => System.exit(0)
       case _ => println("Invalid operation")
     }
--- src/Register.scala
+++ src/Register.scala
@@ -1,21 +1,84 @@
 class Register(val name: String) {
-  val bits = 24
+
   var state = 0
+  var of = 0
+  var sf = 0
+  var pf = 1
 
-  def load(value: Int) = state = value
+  def load(value: Int) = {
+    if (value < Register.min || value > Register.max) {
+      of = 1
+      state = Register.normalizeRange(value)
+    } else {
+      of = 0
+      state = value
+    }
+    sf = if (state < 0) 1 else 0
+    pf = parityFlag()
+  }
 
-  def load(other: Register): Unit = load(other.state)
+  def load(other: Register) = state = other.state
 
-  def xor(value: Int) = state ^= value
+  def xor(value: Int) = {
+    state ^= Register.normalizeRange(value)
+    of = 0
+    sf = if (state < 0) 1 else 0
+    pf = parityFlag()
+  }
 
   def xor(other: Register): Unit = xor(other.state)
 
-  def copy(other: Register) = state = other.state
+  def add(value: Int) = {
+    state += value
+    if (state < Register.min || state > Register.max) {
+      of = 1
+      state = Register.normalizeRange(state)
+    } else {
+      of = 0
+    }
+    sf = if (state < 0) 1 else 0
+    pf = parityFlag()
+  }
 
-  override def toString = name + ": " + Register.formattedBinaryString(state.toBinaryString, bits)
+  def add(other: Register): Unit = add(other.state)
+
+  def copy(other: Register) = {
+    state = other.state
+    sf = other.sf
+  }
+
+  def getFlags() = s"OF:$of SF:$sf PF:$pf"
+
+  def parityFlag() = {
+    var count = 0
+    for (c <- toString) {
+      if (c == '1') count += 1
+    }
+    for (c <- name) {
+      if (c == '1') count -= 1
+    }
+    if (count % 2 == 1) 0 else 1
+  }
+
+  override def toString = {
+    //    val sign = if (state.toBinaryString.length < 32) "0" else "1"
+    val sign = sf
+    val realBits = name + ": " + Register.formattedBinaryString(state.toBinaryString, 24)
+    realBits.substring(0, 4) + sign + realBits.substring(5)
+  }
 }
 
 object Register {
+  val max = (1 << 23) - 1
+  val min = -(1 << 23)
+  val unsignedMax = (1 << 24) - 1
+
+  def normalizeRange(value: Int) = {
+    var v = value
+    while (v > max) v -= unsignedMax
+    while (v < min) v += unsignedMax
+    v
+  }
 
   def formattedBinaryString(binaryString: String, desiredLength: Int): String =
     insertSpaces(formatLength(binaryString, desiredLength), 4)
